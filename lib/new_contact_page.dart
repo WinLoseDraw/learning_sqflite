0ed96eac1ab5bb687099b1844edf1e1961a3c3a9
Nishant Luthra <nishant.luthra30@gmail.com>
2022-05-16 16:24:27 +0530

    Completed new contact page.

--- lib/new_contact_page.dart
+++ lib/new_contact_page.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'database_manager.dart';
 
 class NewContactPage extends StatefulWidget {
   const NewContactPage({Key? key}) : super(key: key);
@@ -10,8 +12,28 @@ class NewContactPage extends StatefulWidget {
 }
 
 class _NewContactPageState extends State<NewContactPage> {
+
+  String errorMessage = "";
+  String inputContactName = "";
+  int inputContactNumber = 0;
+
+  bool validateDetails(String name, int number) {
+    if (name.isEmpty) {
+      errorMessage = "Please enter a name.";
+      return false;
+    }
+    else if (number.toString().length != 10) {
+      errorMessage = "Please enter a valid number.";
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text("New Contact",
@@ -26,7 +48,15 @@ class _NewContactPageState extends State<NewContactPage> {
           margin: EdgeInsets.all(8.0),
           child: ElevatedButton(
               onPressed: () {
-                Navigator.pushReplacementNamed(context, "/");
+                if (validateDetails(inputContactName, inputContactNumber)) {
+                  Provider.of<DatabaseManager>(context, listen: false).insert(Contact(contactName: inputContactName, contactNumber: inputContactNumber)).then(
+                          (value) => Provider.of<DatabaseManager>(context, listen: false).getAllContacts().then((value) => Navigator.pushReplacementNamed(context, "/")));
+                }
+                else {
+                  setState(() {
+                    errorMessage = errorMessage;
+                  });
+                }
               },
               child: Icon(Icons.check),
               style: ButtonStyle(
@@ -62,10 +92,42 @@ class _NewContactPageState extends State<NewContactPage> {
         margin: EdgeInsets.all(20.0),
         child: Column(
           children: [
-            Text("hehe",
+            Text("Contact Name:",
               style: TextStyle(
-                  fontSize: 36.0,
-                  color: Colors.deepOrangeAccent
+                  fontSize: 22.0,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+                  hintText: "Enter contact name",
+                ),
+                onChanged: (text) => inputContactName = text,
+              ),
+            ),
+            Text("Contact Number:",
+              style: TextStyle(
+                  fontSize: 22.0,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+                  hintText: "Enter a 10 digit number",
+                ),
+                keyboardType: TextInputType.number,
+                onChanged: (text) => inputContactNumber = int.parse(text),
+              ),
+            ),
+            Text(errorMessage,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+                fontSize: 22.0,
               ),
             ),
           ],
